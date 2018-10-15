73c5052984292eee97e453e539d8f6cbbb2118be
happyjohnnylin <happyjohnnylin@gmail.com>
2018-10-16 01:47:51 +0800

    modify

--- lib/google_place_api.rb
+++ lib/google_place_api.rb
@@ -15,6 +15,11 @@ module APILibrary
       @cache = cache
     end
 
+    def country_place_id(name)
+      response = get_place(name, 'json')
+      response['json'].parse['candidates'][0]['place_id']
+    end
+
     private
     def place_api_path(params_str, output_format)
       path = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/' + output_format + '?'
@@ -27,7 +32,7 @@ module APILibrary
     
     private
     def get_place(name, output_format)
-      param_key = 'key=' + GOOGLE_MAP_KEY
+      param_key = 'key=' + @api_key
       param_input = 'input=' + name
       param_inputtype = 'inputtype=textquery'
       param_field = 'fields=place_id,geometry'
