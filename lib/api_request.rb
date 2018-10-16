8846df866bc9ed8758e8d63d420a9ceba92456bd
happyjohnnylin <happyjohnnylin@gmail.com>
2018-10-16 13:59:23 +0800

    after rubocop

--- lib/api_request.rb
+++ lib/api_request.rb
@@ -22,13 +22,14 @@ def get_place(name, output_format)
   param_input = 'input=' + name
   param_inputtype = 'inputtype=textquery'
   param_field = 'fields=place_id,geometry'
-  response = HTTP.get(place_api_path([param_key, param_input, param_inputtype, param_field], output_format))
+  param_array = [param_key, param_input, param_inputtype, param_field]
+  response = HTTP.get(place_api_path(param_array, output_format))
   response
 end
 
-def osm_api_path(params_str)
+def osm_api_path(params_array)
   path = 'https://nominatim.openstreetmap.org/search?'
-  params_str.each.with_index do |param, index|
+  params_array.each.with_index do |param, index|
     path += '&' unless index.zero?
     path += param
   end
@@ -44,15 +45,16 @@ def get_country_data(country_name, output_format)
   param_output_format = 'format=' + output_format
   param_country = 'country=' + country_name
   param_polygon_geojson = 'polygon_geojson=1'
-
-  request_url = osm_api_path([param_output_format, param_country, param_polygon_geojson])
+  param_array = [param_output_format, param_country, param_polygon_geojson]
+  request_url = osm_api_path(param_array)
   response = call_osm_url(request_url)
   response
 end
 
 def get_boundaries(country_name)
   response = get_country_data(country_name, 'json')
-  coordinates = JSON.parse(response)[0]["geojson"]["coordinates"].flatten(2)
+  info = JSON.parse(response)
+  coordinates = info[0]['geojson']['coordinates'].flatten(2)
   coordinates
 end
 
@@ -62,19 +64,28 @@ country_results = {}
 
 country_response['json'] = get_country_data('Taiwan', 'json')
 country_info = JSON.parse(country_response['json'])[0]
+country_lon = country_info['lon'].to_s
+country_lat = country_info['lat'].to_s
+country_coordinates = country_info['geojson']['coordinates'].flatten(2)
+
+country_results['place_id'] = country_info['place_id']
+country_results['geo_location'] = country_lon + ', ' + country_lat
+country_results['coordinates'] = country_coordinates
+
+# Invalid request
+country_response['yaml'] = get_country_data('Taiwan', 'yaml')
+
+# Google Place API requests
+# Valid request
 # place_response = {}
 # place_response['json'] = get_place('Taiwan', 'json')
 # place_info = place_response['json'].parse['candidates'][0]
 
-country_results['place_id'] = country_info['place_id']
-country_results['geo_location'] = country_info['lon'].to_s + ', ' + country_info['lat'].to_s
-country_results['coordinates'] = country_info['geojson']['coordinates'].flatten(2)
 # place_results = {}
 # place_results['place_id'] = place_info['place_id']
 # place_results['geo_location'] = place_info['geometry']['location']['lat'].to_s + ', ' + place_info['geometry']['location']['lng'].to_s
 
 # Invalid request
-country_response['yaml'] = get_country_data('Taiwan', 'yaml')
 # place_response['yaml'] = get_place('Taiwan', 'yaml')
 
 File.write('../spec/fixtures/country_response.yml', country_response.to_yaml)
--- lib/osm_data_api.rb
+++ lib/osm_data_api.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module APILibrary
+  # class to get OpenStreetMap data
   class OSMDataAPI
     module Errors
       class NotFound < StandardError; end
       class Unauthorized < StandardError; end
     end
-  
+
     HTTP_ERROR = {
       401 => Errors::Unauthorized,
       404 => Errors::NotFound
@@ -14,33 +17,34 @@ module APILibrary
       @cache = cache
     end
 
-    def osm_api_path(params_str)
+    def osm_api_path(params_array)
       path = 'https://nominatim.openstreetmap.org/search?'
-      params_str.each.with_index do |param, index|
+      params_array.each.with_index do |param, index|
         path += '&' unless index.zero?
         path += param
       end
       path
     end
-    
+
     def call_osm_url(url)
       uri = URI.parse(url)
       Net::HTTP.get(uri)
     end
-    
+
     def get_country_data(country_name, output_format)
       param_output_format = 'format=' + output_format
       param_country = 'country=' + country_name
       param_polygon_geojson = 'polygon_geojson=1'
-    
-      request_url = osm_api_path([param_output_format, param_country, param_polygon_geojson])
+      params_array = [param_output_format, param_country, param_polygon_geojson]
+
+      request_url = osm_api_path(params_array)
       response = call_osm_url(request_url)
       response
     end
-    
+
     def get_boundaries(country_name)
       response = get_country_data(country_name, 'json')
-      coordinates = JSON.parse(response)[0]["geojson"]["coordinates"].flatten(2)
+      coordinates = JSON.parse(response)[0]['geojson']['coordinates'].flatten(2)
       coordinates
     end
   end
