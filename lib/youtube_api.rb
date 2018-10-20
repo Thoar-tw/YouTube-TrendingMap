4a53800c276a1981a25b92557a2cf5f012fcfa17
VivianChiaEn <vivian123.cs02@gmail.com>
2018-10-20 12:40:35 +0800

    modified youtube_api and add spec files

--- lib/youtube_api.rb
+++ lib/youtube_api.rb
@@ -1,36 +1,69 @@
-module APILibrary2
+require 'net/http'
+require 'http'
+require 'json'
+#require_relative '' #parse project data
+#require_relative '' #parse contributor data
+module APILibrary
     class YoutubeAPI
-        def initialize(key, cache: {})
-            @api_key = key
-            @cache = cache
-        end
-        def youtube_api_path(params_str)
-            path = 'https://www.googleapis.com/youtube/v3/search?'+
-            path = path+'key='+ @api_key
-        end
-        def self.videos_list_most_popular(part, params)
-              params_str = ''
-              params.each do |param , index|
-                if index!=''&&params_str!=''
-                    params_str = params_str+ '&' + param.to_s+'='+index.to_s
-                elsif params_str==''
-                    params_str = param.to_s+'='+index.to_s
-                else
-                    break
-                end 
-              end
-            params_str = 'part='+part+'&'+params_str
-            puts params_str
-            response = HTTP.get(youtube_api_path(params_str))
-            print_results(response)
-          end
-    end
+      module Errors
+        class NotFound < StandardError; end
+        class Unauthorized < StandardError; end
+      end
+      HTTP_ERROR = {
+      401 => Errors::Unauthorized,
+      404 => Errors::NotFound
+      }.freeze
+        # def initialize(cache: {})
+        #     @cache = cache
+        # end
 
+        # attach params and keys
+        def self.youtube_api_path(path)
+            'https://www.googleapis.com/youtube/v3/search?'+path+'&key=AIzaSyAD656N9isRF8t11FeFUvizKJCTB07Vrqo'
+        end
+
+        # connect to http server and call api 
+        def self.call_yt_url(url)
+            # result = @cache.fetch(url) do 
+            #     HTTP.headers('Accept' => )
+            @uri = URI.parse(url)
+            return Net::HTTP.get(@uri)
+        end
+
+        # input query and output trending data with json format 
+        def self.videos_list_most_popular(part, params)
+            @params_str = ''
+            # parse query 
+            params.each do |param , index|
+              if index!=''&&@params_str!=''
+                  @params_str = @params_str+ '&' + param.to_s+'='+index.to_s
+              elsif params_str==''
+                  @params_str = param.to_s+'='+index.to_s
+              else
+                  break
+              end 
+            end
+          @params_str = 'part='+part+'&'+@params_str
+          puts @params_str
+          @response = call_yt_url(youtube_api_path(@params_str))
+          @result = JSON.parse(@response)
+          puts @result
+        end
+
+    end
 end 
 
-c = APILibrary2::YoutubeAPI.new
-response = c.videos_list_most_popular('snippet,contentDetails,statistics',
-{'chart': 'mostPopular',
-'region_code': 'US',
-'video_category_id': ''})
-response
+
+
+
+
+# YoutubeAPI.videos_list_most_popular('snippet',
+# {'chart': 'mostPopular',
+# 'region_code': 'US',
+# 'video_category_id': ''})
+
+
+
+
+
+
