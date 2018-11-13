9418e8dfbacbbd39568dd93d82256d378c3e88a6
happyjohnnylin <happyjohnnylin@gmail.com>
2018-11-13 19:49:00 +0800

    move spec_helper

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -12,14 +12,15 @@ require 'minitest/rg'
 
 require 'pry' # for debugging
 
-require_relative '../init.rb'
+require_relative '../../init.rb'
 
 COUNTRY_NAME = 'Taiwan'.freeze
 COUNTRY_CODE = 'tw'.freeze
 DEFAULT_CATEGORY = 0
+DEFAULT_MAX_RESULTS = 10
 
 GOOGLE_CLOUD_KEY = YouTubeTrendingMap::App.config.GOOGLE_CLOUD_KEY
 VIDEO_CATEGORIES = YAML.safe_load(File.read('config/category.yml'))
 
-CORRECT_OSM = YAML.safe_load(File.read(__dir__ + '/fixtures/country_results.yml'))
-CORRECT_YT = YAML.safe_load(File.read(__dir__ + '/fixtures/popular_list_results.yml'))
+CORRECT_OSM = YAML.safe_load(File.read('spec/fixtures/country_results.yml'))
+CORRECT_YT = YAML.safe_load(File.read('spec/fixtures/trending_list_results.yml'))
