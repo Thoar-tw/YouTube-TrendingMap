f678a5fad4308ac7683f63663f7394a119304e6a
happyjohnnylin <happyjohnnylin@gmail.com>
2018-11-13 19:52:25 +0800

    add entity for ranking

---
+++ app/domain/rankings/entities/children/regional_video.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module YouTubeTrendingMap
+  module Entity
+    # Youtube trending videos in a certain region
+    class RegionalVideo < Dry::Struct
+      include Dry::Types.module
+
+      attribute :origin_id,       Strict::String
+      attribute :publish_time,    Strict::String.constrained(
+        format: /(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d).000Z/
+      )
+      attribute :title,           Strict::String
+      attribute :description,     Strict::String
+      attribute :channel_title,   Strict::String
+      attribute :view_count,      Strict::Integer
+      attribute :like_count,      Strict::Integer
+      attribute :dislike_count,   Strict::Integer
+      attribute :embed_link,      Strict::String.constrained(
+        format: %r{https\:\/\/www\.youtube\.com\/embed\/(.*?)}
+      )
+
+      def to_attr_hash
+        to_hash.reject { |key, _| [:id].include? key }
+      end
+    end
+  end
+end
