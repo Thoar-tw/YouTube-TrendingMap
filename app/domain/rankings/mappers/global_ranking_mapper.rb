46c29678d15e630c2cbda6079f16986b8f5c3cbf
happyjohnnylin <happyjohnnylin@gmail.com>
2018-11-13 19:50:50 +0800

    add mappers for ranking entities

---
+++ app/domain/rankings/mappers/global_ranking_mapper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module YouTubeTrendingMap
+  module Mapper
+    # Data structure of trending list queried from Youtube
+    class GlobalRanking
+      def initialize()
+      end
+    end
+  end
+end
