e07b983f20b518d18293b5b123cdb6f491580723
happyjohnnylin <happyjohnnylin@gmail.com>
2018-12-16 03:42:08 +0800

    - add form objects & service objects to top_videos_lists - rubocop fix

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -6,7 +6,7 @@ require 'slim/include'
 
 module YouTubeTrendingMap
   # Web app
-  class App < Roda
+  class App < Roda # rubocop:disable Metrics/ClassLength
     plugin :render, engine: 'slim', views: 'app/presentation/views'
     plugin :assets, css: 'style.css', path: 'app/presentation/assets'
     plugin :halt
@@ -29,9 +29,10 @@ module YouTubeTrendingMap
         # Get viewer's previously seen lists from session
         session[:watching] ||= []
 
-        global_top_videos_list =  Mapper::GlobalTopVideosList
-                                  .new(App.config.GOOGLE_CLOUD_KEY)
-                                  .get(DEFAULT_CATEGORY, DEFAULT_MAX_RESULTS)
+        global_top_videos_list =
+          Mapper::GlobalTopVideosList
+          .new(App.config.GOOGLE_CLOUD_KEY)
+          .get(DEFAULT_CATEGORY, DEFAULT_MAX_RESULTS)
 
         if global_top_videos_list.nil?
           flash[:error] = 'global top videos list is nil'
@@ -47,7 +48,7 @@ module YouTubeTrendingMap
       routing.on 'hot_videos' do # rubocop:disable Metrics/BlockLength
         routing.is do # rubocop:disable Metrics/BlockLength
           # POST /trending_map/
-          routing.post do
+          routing.post do # rubocop:disable Metrics/BlockLength
             # If user request from country name field, mapping it to region code
             unless routing.params['country_name'].nil?
               region_code = COUNTRY_CODES[routing.params['country_name']]
@@ -70,14 +71,15 @@ module YouTubeTrendingMap
 
             request[:region_code] = region_code_request[:region_code]
             request[:category_id] = category_id_request[:category_id]
-            hot_videos_list_result = Services::GetHotVideosList.new.call(request)
+            puts  'Getting hot videos list for ' + request[:region_code] +
+                  ' in category ' + request[:category_id]
 
+            hot_videos_list_result = Services::GetHotVideosList.new.call(request)
             if hot_videos_list_result.failure?
               flash[:error] = hot_videos_list_result.failure
               routing.redirect '/hot_videos'
             end
 
-            puts 'Getting hot videos list for ' + request[:region_code] + ' in category ' + request[:category_id]
             hot_videos_list = hot_videos_list_result.value!
             view_hot_videos_list = Views::HotVideosList.new(hot_videos_list)
 
@@ -94,31 +96,34 @@ module YouTubeTrendingMap
       end
 
       routing.on 'top_videos' do # rubocop:disable Metrics/BlockLength
-        routing.is do # rubocop:disable Metrics/BlockLength
+        routing.is do
           routing.redirect '/top_videos/global'
         end
 
-        routing.on 'global' do
+        routing.on 'global' do # rubocop:disable Metrics/BlockLength
           routing.post do
-            # user enter specific region and category
-            category_id = routing.params['category_id'].nil? ? DEFAULT_CATEGORY : routing.params['category_id']
-
-            begin
-              global_top_videos_list =  YouTubeTrendingMap::Mapper::GlobalTopVideosList
-                                        .new(App.config.GOOGLE_CLOUD_KEY)
-                                        .get(category_id, 10)
-
-              if global_top_videos_list.nil?
-                flash[:error] = 'global top videos list is nil'
-                routing.redirect '/top_videos'
-              end
-
-              view_global_top_videos_list = Views::TopVideosList.new(global_top_videos_list)
-            rescue StandardError => error
-              puts error
-              flash[:error] = 'Having trouble getting global top videos list'
+            # user enter specific category
+            category_id_request = Forms::CategoryIdRequest.call(routing.params)
+            if category_id_request.failure?
+              error = category_id_request.errors[:category_id][0]
+              puts 'category_id_request:' + error
+              flash[:error] = error
               routing.redirect '/top_videos/global'
             end
+
+            global_top_videos_list_result =
+              Services::GetGlobalTopVideosList.new.call(category_id_request)
+            if global_top_videos_list_result.failure?
+              failure = global_top_videos_list_result.failure
+              puts 'global_top_videos_list_result: ' + failure
+              flash[:error] = failure
+              routing.redirect '/top_videos/global'
+            end
+
+            global_top_videos_list =
+              global_top_videos_list_result.value!
+            view_global_top_videos_list =
+              Views::TopVideosList.new(global_top_videos_list)
 
             routing.redirect '/top_videos/global'
           end
@@ -131,30 +136,48 @@ module YouTubeTrendingMap
         end
 
         routing.on 'continent' do # rubocop:disable Metrics/BlockLength
-          routing.post do
-            # user enter specific region and category
-            continent = routing.params['continent'].downcase
-            category_id = routing.params['category_id'].nil? ? DEFAULT_CATEGORY : routing.params['category_id']
-
-            begin
-              continent_top_videos_list =
-                YouTubeTrendingMap::Mapper::ContinentTopVideosList
-                .new(App.config.GOOGLE_CLOUD_KEY)
-                .get(continent, category_id, 10)
-              
-              puts '~~~'
-              if continent_top_videos_list.nil?
-                puts 'continent_top_videos_list.nil'
-                flash[:error] = 'continent top videos list is nil'
-                routing.redirect '/top_videos/continent'
-              end
-
-              view_continent_top_videos_list = Views::TopVideosList.new(continent_top_videos_list)
-            rescue StandardError => error
-              puts error
-              flash[:error] = 'Having trouble getting continent top videos list'
+          routing.post do # rubocop:disable Metrics/BlockLength
+            ### user enter specific region and category
+            # downcase the input for continent name
+            routing.params['continent_name'] =
+              routing.params['continent_name'].downcase
+            # Check if continent request matches it's valid string values
+            continent_name_request =
+              Forms::ContinentNameRequest.call(routing.params)
+            if continent_name_request.failure?
+              error = continent_name_request.errors[:continent_name][0]
+              puts 'continent_name_request: ' + error
+              flash[:error] = error
               routing.redirect '/top_videos/continent'
             end
+
+            # Check if category id request matches it's valid regex
+            category_id_request = Forms::CategoryIdRequest.call(routing.params)
+            if category_id_request.failure?
+              error = category_id_request.errors[:category_id][0]
+              puts 'category_id_request: ' + error
+              flash[:error] = error
+              routing.redirect '/top_videos/continent'
+            end
+
+            request[:continent_name] = continent_name_request[:continent_name]
+            request[:category_id] = category_id_request[:category_id]
+            puts  'Getting top videos list for ' + request[:continent_name] +
+                  ' in category ' + request[:category_id]
+
+            continent_top_videos_list_result =
+              Services::GetContinentTopVideosList.new.call(request)
+            if continent_top_videos_list_result.failure?
+              failure = continent_top_videos_list_result.failure
+              puts 'continent_top_videos_list_result: ' + failure
+              flash[:error] = failure
+              routing.redirect '/top_videos/continent'
+            end
+
+            continent_top_videos_list =
+              continent_top_videos_list_result.value!
+            view_continent_top_videos_list =
+              Views::TopVideosList.new(continent_top_videos_list)
 
             routing.redirect '/top_videos/continent'
           end
@@ -162,16 +185,55 @@ module YouTubeTrendingMap
           view 'continent_top_videos', locals: {
             mapbox_token: App.config.MAPBOX_TOKEN,
             continent_top_videos_list: view_continent_top_videos_list,
-            countries: COUNTRIES,
+            continents: CONTINENTS,
             categories: CATEGORIES
           }
         end
 
-        routing.on 'country' do
+        routing.on 'country' do # rubocop:disable Metrics/BlockLength
           routing.post do # rubocop:disable Metrics/BlockLength
-            # user enter specific region and category
-            
-            view_country_top_videos_list = nil
+            ### user enter specific region and category
+            # If user request from country name field, mapping it to region code
+            unless routing.params['country_name'].nil?
+              region_code = COUNTRY_CODES[routing.params['country_name']]
+              routing.params['region_code'] = region_code
+            end
+
+            # Check if region code request matches it's valid regex
+            region_code_request = Forms::RegionCodeRequest.call(routing.params)
+            if region_code_request.failure?
+              error = region_code_request.errors[:region_code][0]
+              puts 'region_code_request: ' + error
+              flash[:error] = region_code_request.errors[:region_code][0]
+              routing.redirect '/top_videos/country'
+            end
+
+            # Check if category id request matches it's valid regex
+            category_id_request = Forms::CategoryIdRequest.call(routing.params)
+            if category_id_request.failure?
+              error = category_id_request.errors[:category_id][0]
+              puts 'category_id_request: ' + error
+              flash[:error] = category_id_request.errors[:category_id][0]
+              routing.redirect '/top_videos/country'
+            end
+
+            request[:region_code] = region_code_request[:region_code]
+            request[:category_id] = category_id_request[:category_id]
+            country_top_videos_list_result =
+              Services::GetCountryTopVideosList.new.call(request)
+
+            if country_top_videos_list_result.failure?
+              failure = country_top_videos_list_result.failure
+              puts 'country_top_videos_list_result: ' + failure
+              flash[:error] = failure
+              routing.redirect '/top_videos/country'
+            end
+
+            puts  'Getting top videos list for ' + request[:region_code] +
+                  ' in category ' + request[:category_id]
+            country_top_videos_list = country_top_videos_list_result.value!
+            view_country_top_videos_list =
+              Views::TopVideosList.new(country_top_videos_list)
 
             routing.redirect '/top_videos/country'
           end
