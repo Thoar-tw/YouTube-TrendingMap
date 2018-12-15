d95dff300137df057c388e957e951efcde87c1a9
happyjohnnylin <happyjohnnylin@gmail.com>
2018-12-16 03:46:31 +0800

    add continent yml file

--- config/environment.rb
+++ config/environment.rb
@@ -46,10 +46,11 @@ module YouTubeTrendingMap
     CATEGORIES = YAML.safe_load(File.read('config/category.yml'))
 
     # Files for Country mapping
-    COUNTRIES = YAML.safe_load(File.read('config/country.yml'))
+    COUNTRIES = YAML.safe_load(File.read('config/countries.yml'))
     COUNTRY_CODES = YAML.safe_load(
       File.read('config/country_code_iso_alpha2.yml')
     )
+    CONTINENTS = YAML.safe_load(File.read('config/continents.yml'))
     CONTINENT_COUNTRY_CODES = JSON.parse(
       File.read('config/continent_country_codes.json')
     )
