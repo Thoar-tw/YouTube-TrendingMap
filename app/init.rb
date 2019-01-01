0c35c88a5bea3bcf157518fdc093b8aa533fae00
happyjohnnylin <happyjohnnylin@gmail.com>
2019-01-01 17:44:37 +0800

    fix init

--- app/init.rb
+++ app/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: false
 
-folders = %w[application domain infrastructure]
+folders = %w[application infrastructure presentation]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
